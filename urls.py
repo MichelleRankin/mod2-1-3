b8a7ce2ce162b47eb67ee2fbeb671e496a9ca452
MichelleRankin <89100717+MichelleRankin@users.noreply.github.com>
2021-09-24 15:29:19 -0500

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import hello_view
+from app.views import hello_view, home_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home_view),
     path("<name>/", hello_view),
 ]
+
